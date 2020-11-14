4d13dd71ffea1d235130fe93d86fb952cd5c7e87
modass-cppu <modass24@gmail.com>
2020-11-14 16:29:58 -0600

    Platform added

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var platform;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -26,6 +27,8 @@ function setup(){
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
+
+    platform = new Ground(180, 305, 300, 170)
 
     log3 =  new Log(810,180,300, PI/2);
 
@@ -59,4 +62,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
